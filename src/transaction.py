6d003edb8e761b3217c44af7f994d2c02f87ff65
OlyaEf <efimovskikh@gmail.com>
2023-04-26 10:42:45 +0100

    fix docstrings to transaction.py

--- src/transaction.py
+++ src/transaction.py
@@ -7,14 +7,14 @@ class Transaction:
     """
     Класс Transaction представляет информацию о финансовой транзакции.
     Атрибуты:
-    id (int): идентификатор транзакции.
-    state (str): статус транзакции, возможные значения: EXECUTED, CANCELED.
-    date (str): дата выполнения транзакции в формате ГГГГ-ММ-ДДTЧЧ:ММ:СС.МСК.
-    amount (float): сумма операции.
-    currency (str): название валюты операции.
-    description (str): описание операции.
-    from_account (str): номер счета или карты, с которого была произведена операция.
-    to_account (str): номер счета или карты, на который была произведена операция.
+    id (int): Идентификатор транзакции.
+    state (str): Статус транзакции, возможные значения: EXECUTED, CANCELED.
+    date (str): Дата выполнения транзакции в формате ГГГГ-ММ-ДДTЧЧ:ММ:СС.МСК.
+    amount (float): Сумма операции.
+    currency (str): Название валюты операции.
+    description (str): Описание операции.
+    from_account (str): Номер счета или карты, с которого была произведена операция.
+    to_account (str): Номер счета или карты, на который была произведена операция.
     """
     id: int
     state: str
@@ -26,12 +26,11 @@ class Transaction:
     to_account: str
 
 
-def load_tran_transactions_from_json(file_path: str) -> List[Transaction]:
+def load_transactions_from_json(file_path: str) -> List[Transaction]:
     """
-    Функция загрузки списка транзакций из JSON файла
-    и преобразование его в список объектов класса Transaction.
-    :param file_path: JSON файл.
-    :return: список объектов класса Transaction.
+    Функция возвращает список объектов класса Transaction.
+    :param file_path: Путь к файлу.
+    :return: Список объектов класса Transaction.
     """
     import json
     with open(file_path, 'r', encoding='utf-8') as f:
@@ -48,14 +47,14 @@ def load_tran_transactions_from_json(file_path: str) -> List[Transaction]:
             from_account=item['from'],
             to_account=item['to']
         ))
-        return transactions
+    return transactions
 
 
 def get_last_transactions(transactions: List[Transaction], count: int = 5) -> List[Transaction]:
     """
     Функция получения последней транзакции из списка объектов класса Transaction.
-    :param transactions: список объектов класса Transaction.
-    :param count: количество транзакций.
-    :return: список объектов класса Transaction.
+    :param transactions: Список объектов класса Transaction.
+    :param count: Количество транзакций.
+    :return: Список объектов класса Transaction.
     """
     return sorted(transactions, key=lambda t: t.date, reverse=True)[:count]
